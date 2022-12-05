e878542f25d8f62dd85f1ff9dcea01941c9afdf9
Roman <roman-ksv@mail.ru>
2022-12-05 03:37:17 +0300

    git commit

--- server/models/User.js
+++ server/models/User.js
@@ -2,42 +2,14 @@ import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema(
     {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        firstname: {
-            type: String,
-            default: ''
-        },
-        secondname: {
-            type: String,
-            default: ''
-        },
-        city: {
-            type: String,
-            default: ''
-        },
-        phonenumber: {
-            type: Number,
-            default: ''
-        },
-        
-        admin: {type: Boolean, default: false },
-        
-
-        posts: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Post',
-            },
-        ],
-    },
+username: {type: String,required: true, unique: true,},
+password: {type: String,required: true,},
+firstname: {type: String, default: ''},
+secondname: {type: String,default: ''},
+city: {type: String,default: '' },
+phonenumber: {type: Number,default: ''},
+admin: {type: Boolean, default: false },
+posts: [{type: mongoose.Schema.Types.ObjectId,ref: 'Post',},],},
     { timestamps: true },
 )
 
